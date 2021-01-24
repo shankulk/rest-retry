fbf8a23eeb726f0e152c75c2b343d4a1c37653ce
Shantanu Kulkarni <shankulk@gmail.com>
2021-01-24 01:41:06 +0000

    Initial commit

---
+++ src/main/java/com/shankulk/RestRetryApplication.java
@@ -0,0 +1,13 @@
+package com.shankulk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestRetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankulk/RestRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankulk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
